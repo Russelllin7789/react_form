610f11c96af9a022255855faacfdc2eea5224048
RussellLin <superbr7789@gmail.com>
2022-02-02 14:55:45 +0800

    Add some basic validation with useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useState } from 'react'
+
 const SimpleInput = (props) => {
+  const [name, setName] = useState('')
+  const [nameIsValid, setNameIsValid] = useState(true)
+
+  const nameInputHandler = (event) => {
+    setName(event.target.value)
+  }
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault()
+    if (name.trim().length < 1) {
+      setNameIsValid(false)
+      return
+    }
+
+    setNameIsValid(true)
+    console.log('user name:', name)
+    setName('')
+  }
+
+  const nameInputClasses = nameIsValid ? 'form-control' : 'form-control invalid'
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={nameInputHandler} value={name} />
+        {!nameIsValid && <p className='error-text'>Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
